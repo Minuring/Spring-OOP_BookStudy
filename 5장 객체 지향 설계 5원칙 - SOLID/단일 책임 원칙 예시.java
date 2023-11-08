8cecb4c3fc866e327d0d98b84373b38f4d2aa03d
Minuring <mw9505@gmail.com>
2023-11-08 15:46:50 +0900

    Add : SRP 예시

---
+++ 5장 객체 지향 설계 5원칙 - SOLID/단일 책임 원칙 예시.java
@@ -0,0 +1,60 @@
+/*
+Book 클래스 : 책과 관련된 정보
+Print 기능이 확장될 경우 Book클래스를 수정해야함
+== Print 기능의 확장에 닫혀있음 (ORP까지 위반)
+ */
+public class Book {
+    public String getTitle() {
+        return "A Great Book";
+    }
+
+    public String getAuthor() {
+        return "John Doe";
+    }
+
+    public void printCurrentPage() {
+        System.out.println("current page content");
+        //html 프린터를 넣고자 한다.
+    }
+}
+/*
+Book 클래스의 책임을 나눈다.
+이제 다양한 출력 옵션이 생겨도 Book 클래스를 수정할 필요가 없어진다.
+Printer 인터페이스를 각기 다르게 구현하면 되기때문
+ */
+public class Book {
+
+    public String getTitle() {
+        return "A Great Book";
+    }
+
+    public String getAuthor() {
+        return "John Doe";
+    }
+
+    public void getCurrentPage() {
+        System.out.println("current page content");
+    }
+
+}
+
+public interface Printer {
+    void printPage();
+}
+
+public class PlainTextPrinter implements Printer {
+
+    public void printPage() {
+        System.out.println("this is plain text printer");
+    }
+
+}
+
+public class HtmlPrinter implements Printer {
+
+    public void printPage() {
+        System.out.println("this is html printer");
+    }
+
+}
+
